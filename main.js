9c0950d6bd892c26357cbb964d47bda9d492e7ba
AnnaGruber263 <anna.gruber@uibk.ac.at>
2024-04-11 18:23:31 +0200

    var zu let, Koordinaten geändert, Maßstab eingefügt, geoJSON eingefügt

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-var map = L.map('map').setView([47.776389, 13.433611], 13);
+let lat = 47.776389
+let lng = 13.433611
+let zoom = 13
+
+let map = L.map('map', {
+    center: [lat, lng],
+    zoom: zoom
+});
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([47.776389, 13.433611]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+L.control.scale({
+    imperial: false
+}).addTo(map);
+
+
+
+
+L.geoJSON(jsondata, {}).bindPopup(function (layer) {
+    console.log(layer.feature.properties)
+    return `
+    <h2>${layer.feature.properties.name}</h2>
+    <ul>
+    <li>Breite: ${layer.feature.geometry.coordinates[1].toFixed(5)} </li>
+    <li>Länge: ${layer.feature.geometry.coordinates[0].toFixed(5)} </li>
+    <li><a href="https://${layer.feature.properties.user}.github.io/top">Etappenseite>/
+    a></li>
+    <li><a href="${layer.feature.properties.wikipedia}">Wikipediaseite</li>
+
+    </ul>
+    `;
+}).addTo(map);
+
+
